8859e433224239290be7f766f90233056764cfdf
Jacob Lyles <jacob.lyles@gmail.com>
2016-12-27 19:57:06 -0800

    multisig creation and redemption functions wip

--- xor.py
+++ xor.py
@@ -6,6 +6,9 @@ import argparse
 import sys
 import hashlib
 from hashlib import sha256
+import random
+import subprocess
+import json
 
 #### Begin portion copyrighted by David Keijser #####
 
@@ -184,7 +187,7 @@ def read_seed_interactive(min_length):
   return seed
 
 
-#### main functions #####
+#### main private key creation functions #####
 
 def seed_to_WIF(seed):
   seed_80 = "80" + seed
@@ -211,9 +214,9 @@ def checksum(s):
   return h2[0:8]
 
 
-def main_interactive(dice_length = 62, seed_length = 20):
+def wif_interactive(dice_length = 62, seed_length = 20):
   # dice_string = read_dice_interactive(dice_length)
-  dice_string = "123425362526352316253516215216351525112515236121213423423412312"
+  dice_string = "223425362526352316253516215216351525112515236121213423423412312"
   
   dice_hash = hashSha256(dice_string)
 
@@ -234,6 +237,77 @@ def main_interactive(dice_length = 62, seed_length = 20):
   return seed_to_WIF(combined_seed)
 
 
+#### multisig creation functions #####
+
+def get_address_for_privkey(privkey):
+  """A method for retrieving the address associated with a private key from bitcoin core
+     <privkey> - a bitcoin private key in WIF format"""
+
+  # Arbitrary label. A unique label ensures that we will get back only one public key 
+  # when we call the "getaddressesbyaccount" rpc later
+  label = random.randint(0, 2**128)
+  subprocess.call("bitcoin-cli importprivkey {0} {1}".format(privkey, label), shell=True)
+  addresses = subprocess.check_output("bitcoin-cli getaddressesbyaccount {0}".format(label), shell=True)
+  addresses_json = json.loads(addresses)
+  return addresses_json[0]
+
+def get_multisig_interactive(m,n):
+  """Asks user for n bitcoin addresses. Returns an m of n multisig address and redeem script"""
+  addrs = []
+  while len(addrs) < n:
+    print "enter address #{0}:".format(len(addrs) + 1)
+    new_addr = raw_input()
+    addrs.append(new_addr)
+  
+  addrs_string = json.dumps(addrs)
+  label = random.randint(0, 2**128)
+
+  argstring = "{0} '{1}'".format(m, addrs_string)
+
+  results = subprocess.check_output("bitcoin-cli createmultisig {0}".format(argstring), shell=True)
+  
+  return json.loads(results)
+
+
+#### multisig redemption functions ####
+
+def multisig_gen_trx(dest_addr, amount, redeem_script, in_txid, in_vout, in_ouput_script, privkeys):
+  data_1 = [{
+   "txid": in_txid,
+   "vout": in_vout
+  }]
+  dest_data_1 = {
+    dest_addr: amount
+  }
+  argstring_1 = "'{0}' '{1}'".format(json.dumps(data_1), json.dumps(dest_data_1))
+  print argstring_1
+
+  tx_hex = subprocess.check_output("bitcoin-cli createrawtransaction {0}".format(argstring_1), shell=True).strip()
+
+  print tx_hex
+  
+  data_2 = [{
+    "txid": in_txid,
+    "vout": in_vout,
+    "scriptPubKey": in_ouput_script,
+    "redeemScript": redeem_script
+  }]
+
+  argstring_2 = "{0} '{1}' '{2}'".format(tx_hex, json.dumps(data_2), json.dumps(privkeys))
+  signed_tx_hex = subprocess.check_output("bitcoin-cli signrawtransaction {0}".format(argstring_2), shell=True).strip()
+
+  print signed_tx_hex
+
+  return signed_tx_hex
+
+
 if __name__ == "__main__": 
 
-  main_interactive()  
+  # wif_interactive()
+  # print get_address_for_privkey("5JHLk1zFzbDY7jJS6RdmZYqHEv5J89NpVC7teru7xrhghqo53mf")
+
+  # print get_multisig_interactive(1,2)
+  print multisig_gen_trx("14bdjdoN2orodNcPaq5iVd8aSToKKn7cnN", 0.00020000, "51410421167f7dac2a159bc3957e3498bb6a7c2f16874bf1fbbe5b523b3632d2c0c43f1b491f6f2f449ae45c9b0716329c0c2dbe09f3e5d4e9fb6843af083e222a70a441043704eafafd73f1c32fafe10837a69731b93c0179fa268fc325bdc08f3bb3056b002eac4fa58c520cc3f0041a097232afbe002037edd5ebdab2e493f18ef19e9052ae",
+    "5a507797946da2310ddbc6820e1115ca6d640ab499cb1748db316030c269cb62", 0, "a914f1e3f2ba9971cf5f82daf0f8fe6b4c999f4dfc3587", ["5JHLk1zFzbDY7jJS6RdmZYqHEv5J89NpVC7teru7xrhghqo53mf", "5JH4aEVfQgfjC4tZ394sWyGv8NqLMPd3XmwVtnJ1cKwksYQqan6"])
+
+
