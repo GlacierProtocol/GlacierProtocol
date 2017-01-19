5fdee10a7d44173bae45cb047a775623cfdf5da1
James Hogan <jhogan@jhogan.net>
2017-01-19 12:39:21 -0800

    Delete gen_sums.sh
    putting inline in release procedure gdoc

--- gen_sums.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm SHA256SUMS SHA256SUMS.sig
-shasum -a 256 -b xor.py >> SHA256SUMS
-shasum -a 256 -b README.md >> SHA256SUMS
-shasum -a 256 -b BitcoinHighSecurityStorageProtocolv0.1AlphaWORKINGCOPY.pdf >> SHA256SUMS
-gpg --output SHA256SUMS.sig --armor --detach-sig SHA256SUMS
