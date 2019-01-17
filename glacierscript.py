38d053ddab589e4afee327a133e6377785cbf728
Bitcoin Hodler <bitcoinhodler@safe-mail.net>
2019-01-17 01:21:37 +0000

    Don't make assumptions about length of args
    This will enable me to remove empty ("") args as unnecessary next.

--- glacierscript.py
+++ glacierscript.py
@@ -103,7 +103,8 @@ def run_subprocess(sub_func, exe, *args, **kwargs):
     """
     silent = kwargs.pop('silent', False)
     if kwargs: raise TypeError('Unexpected **kwargs: %r' % kwargs)
-    full_cmd = "{0} {1} {2} {3}".format(exe, cli_args, *args)
+    mylist = [exe, cli_args] + list(args)
+    full_cmd = " ".join(mylist)
     cmd_list = shlex.split(full_cmd)
     subprocess_args = {}
     devnull = None
