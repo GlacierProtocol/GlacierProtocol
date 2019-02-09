51f191dcd3f557cca0c902722a1b8d239f2a3b4b
Bitcoin Hodler <bitcoinhodler@safe-mail.net>
2019-02-09 06:35:54 +0000

    Simplify arg parse

--- glacierscript.py
+++ glacierscript.py
@@ -860,8 +860,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-n", type=int, help="Number of total keys required in an m-of-n multisig address creation (default m-of-n = 1-of-2)", default=2)
     parser.add_argument('--testnet', type=int, help=argparse.SUPPRESS)
-    parser.add_argument('-v', action='store_const', default=0, dest='verbose_mode', const=1,
-                        help='increase output verbosity')
+    parser.add_argument('-v', action='store_true', dest='verbose_mode', help='increase output verbosity')
     args = parser.parse_args()
 
     verbose_mode = args.verbose_mode
