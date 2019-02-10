1e1effd69c5dd3518ce0264e573a535e0dbd7a62
Bitcoin Hodler <bitcoinhodler@safe-mail.net>
2019-02-10 05:40:58 +0000

    Add safety check of importmulti results

--- glacierscript.py
+++ glacierscript.py
@@ -447,6 +447,9 @@ def teach_address_to_wallet(source_address, redeem_script, privkeys):
         if source_address == decoded_script["segwit"]["p2sh-segwit"]:
             import_this["redeemscript"] = decoded_script["segwit"]["hex"]
     results = bitcoin_cli_json("importmulti", json.dumps([import_this]))
+    if not all(result["success"] for result in results) or \
+       any("warnings" in result for result in results):
+        raise Exception("Problem importing address to wallet")
 
 
 def sign_transaction(source_address, keys, redeem_script, unsigned_hex, input_txs):
